3eddfe8261a729a361088346bfff34389eefbebf
ProductiveAnalytics <LChawathe@ProductiveAnalytics.com>
2018-02-18 23:37:22 -0600

    minor import cleanup

--- server/src/main/java/com/okta/developer/demo/CarRepository.java
+++ server/src/main/java/com/okta/developer/demo/CarRepository.java
@@ -2,7 +2,7 @@ package com.okta.developer.demo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource
 //@CrossOrigin(origins = "http://localhost:4200")
