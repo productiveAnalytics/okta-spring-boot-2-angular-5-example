a95313d41dfdac956160a18007246810ec00658a
ProductiveAnalytics <LChawathe@ProductiveAnalytics.com>
2018-03-29 01:05:32 -0500

    Added explicit getter/setter for id so that Angular UI can render car.id

--- server/src/main/java/com/okta/developer/demo/Car.java
+++ server/src/main/java/com/okta/developer/demo/Car.java
@@ -1,6 +1,7 @@
 package com.okta.developer.demo;
 
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Entity;
 
@@ -9,7 +10,12 @@ public class Car {
     @Id
     @GeneratedValue
     private Long id;
+    
+    @NotNull
     private String name;
+    
+    public Long getId() {return this.id;}
+    public void setId(Long _id) {this.id = _id;}
     
     public String getName() {return this.name;}
     public void setName(String n) {this.name = n;}
